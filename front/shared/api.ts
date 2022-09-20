956615376de432633eab9de3ffaf7d55de528d0b
Donathan Queiros <66445710+donathanqueiros@users.noreply.github.com>
2022-09-20 13:50:23 -0300

    fix url

--- front/shared/api.ts
+++ front/shared/api.ts
@@ -1,7 +1,7 @@
 //create axios instance with base url
 import axios from "axios";
 
-const baseURL = process.env["NEXT_PUBLIC_VERCEL_URL"];
+const baseURL = process.env["BASE_URL"];
 console.log(baseURL);
 
 const api = axios.create({
