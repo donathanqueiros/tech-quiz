e7b41ec51403c816301e016069de19aa00a38515
teste <teste@teste.com>
2022-11-29 16:14:48 -0300

    feat: update data - 7INNa

---
+++ front/src/data/index.ts
@@ -0,0 +1,5 @@
+import novo_dado from "./novo_dado";
+import backend from "./backend";
+import frontend from "./frontend";
+import { Road } from "./road";
+export default [novo_dado, backend, frontend] as Road[];
